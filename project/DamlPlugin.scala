9946feceecad6fe859de16b675cf06b30fd2aca0
Itai Segall <itai.segall@digitalasset.com>
2026-04-14 21:45:40 +0000

    [ci] show the output
    Signed-off-by: Itai Segall <itai.segall@digitalasset.com>

--- project/DamlPlugin.scala
+++ project/DamlPlugin.scala
@@ -471,6 +471,7 @@ object DamlPlugin extends AutoPlugin {
       log,
       optCwd = Some(projectDirectory),
       extraEnv = damlLibsEnv, // env variable set so that damlc finds daml-script dar
+      streamToStdout = true,
     )
 
     val currentDar = outputDirectory / s"$damlProjectName-current.dar"
